bd4448cc8d2f5021c4794bcfb34086d55f9a7958
Prashobh Nair <prashobhmanojnair@gmail.com>
2026-02-22 17:26:45 +0530

    Fixed Airlabs auth error

--- api/flights.js
+++ api/flights.js
@@ -1,10 +1,14 @@
 export default async function handler(req, res) {
-  const response = await fetch(
-    "https://airlabs.co/api/v9/flights?api_key=02f0e69e-ebf7-4c3b-bd30-101a94d800d3"
-  );
+  try {
+    const response = await fetch(
+      "https://airlabs.co/api/v9/flights?api_key=02f0e69e-ebf7-4c3b-bd30-101a94d800d3"
+    );
 
-  const data = await response.json();
+    const data = await response.json();
 
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.status(200).json(data);
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Server error" });
+  }
 }
