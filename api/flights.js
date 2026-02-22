f71d88f46f79c9d23f8f5fd1c5994fd28c9ac1c2
Prashobh Nair <prashobhmanojnair@gmail.com>
2026-02-22 17:17:01 +0530

    Added Airlabs API key

--- api/flights.js
+++ api/flights.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   const response = await fetch(
-    "https://airlabs.co/api/v9/flights?02f0e69e-ebf7-4c3b-bd30-101a94d800d3"
+    "https://airlabs.co/api/v9/flights?api_key=02f0e69e-ebf7-4c3b-bd30-101a94d800d3"
   );
 
   const data = await response.json();
